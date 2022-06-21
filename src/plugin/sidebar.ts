c3596d4c3958600e5b083506776e6d7e2b986ef4
zmh69695328 <736503352@qq.com>
2022-06-21 16:11:58 +0800

    fix:add directory arrow down and right

--- src/plugin/sidebar.ts
+++ src/plugin/sidebar.ts
@@ -1,32 +1,23 @@
 import { NodeObj } from ".."
 
-let cnt:number
-
-function queryList(nodeData:NodeObj):string{
-    cnt++
+function queryList(nodeData:NodeObj,cnt:number):string{
     let res:string=''
-    if(cnt>2){
-        
-    }
     for(let i =0;i<nodeData.children?.length;i++){
         // res+=queryList(nodeData.children[i])`
-        res +=`<li class="sidebar-links">${queryList(nodeData.children[i])}</li>`
+        res +=`<li class="sidebar-links">${queryList(nodeData.children[i],cnt+1)}</li>`
     }
     if(!nodeData?.children||!nodeData.children.length){
         return `<p class="sidebar-title"><span>${nodeData.topic}</span></p>`
     }else{
-        return `<ul class="sidebar-heading open"><p class="sidebar-title"><span>${nodeData.topic}</span> <span class="arrow down"></span></p>${res}</ul>`
+        return `<ul class="sidebar-heading open"><p class="sidebar-title"><span>${nodeData.topic}</span> <span class="arrow ${cnt<2?'down':'right'}"></span></p>${res}</ul>`
     }
     
 }
 
 export default function(mind){
-    cnt = 0
     const sidebar = document.createElement('sidebar')
     const nodeData=mind.nodeData
-    sidebar.innerHTML = `
-   ${queryList(nodeData)}
-    `
+    sidebar.innerHTML = `${queryList(nodeData,0)}`
     mind.container.append(sidebar)
 
 }
